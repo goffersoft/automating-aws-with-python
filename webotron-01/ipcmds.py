dc7f45401484b577eb0d2ebb464bb5886e2cd236
goffer123 <goffersoft@gmail.com>
2020-05-08 15:59:51 -0400

    clenaup

--- webotron-01/ipcmds.py
+++
@@ -1,7 +0,0 @@
-# coding: utf-8
-import boto3
-session = boto3.Session(profile_name='python_automation')
-ec2_resource = session.resource('ec2')
-s3_resource = session.resource('s3')
-ec2_client = session.client('ec2')
-s3_client = session.client('s3')

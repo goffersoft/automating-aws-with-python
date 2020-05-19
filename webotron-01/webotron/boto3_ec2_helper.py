f6b1983a80cab665bdeb8e37cc61c4d308c6c734
goffer123 <goffersoft@gmail.com>
2020-05-18 20:27:01 -0400

    bugfixes/cleanup

--- webotron-01/webotron/boto3_ec2_helper.py
+++ webotron-01/webotron/boto3_ec2_helper.py
@@ -6,12 +6,12 @@
 
 def get_ec2_resource(session):
     """Get ec2 resource."""
-    return session.get_resource(session, 'ec2')
+    return session.get_resource('ec2')
 
 
 def get_ec2_client(session):
     """Get ec2 client."""
-    return session.get_client(session, 'ec2')
+    return session.get_client('ec2')
 
 
 if __name__ == '__main__':

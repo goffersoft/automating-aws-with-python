dfa52137508a861ce19df8286794a6f45985f77f
goffer123 <goffersoft@gmail.com>
2020-06-01 18:16:54 -0400

    prepare awsbot for packaging and distribution

---
+++ awsbot-01/setup.py
@@ -0,0 +1,30 @@
+#! /usr/bin/python
+# -*- coding:utf-8 -*-
+
+"""Python Packaging."""
+
+from setuptools import setup
+
+setup(
+    name='awsbot-20',
+    version='0.1',
+    author='Goffer',
+    author_email='goffersoft@gmail.com',
+    description='awsbot 20 is a cli based automation tool/library for AWS',
+    license='GPLv3+',
+    packages=['awsbot'],
+    package_data={
+        "": ['config/*',
+             'templates/policy/*',
+             'templates/www/*'],
+    },
+    include_package_data=True,
+    url='https://github.com/goffersoft/automating-aws-with-python',
+    install_requires=['click',
+                      'boto3',
+                      'html5lib'],
+    entry_points='''
+        [console_scripts]
+        awsbot=awsbot.awsbot:awsbot
+    '''
+)

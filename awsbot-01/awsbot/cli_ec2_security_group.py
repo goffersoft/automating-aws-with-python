19a7ad2db8f2c3ab1b267aca2dcc8818f8bbcd07
goffer123 <goffersoft@gmail.com>
2020-06-09 18:16:04 -0400

    add cli command to create / delete security groups; update list command to filter by group names/ids; cleanup; bugfixes

--- awsbot-01/awsbot/cli_ec2_security_group.py
+++ awsbot-01/awsbot/cli_ec2_security_group.py
@@ -21,16 +21,52 @@ def ec2_security_group(session):
 
 
 @ec2_security_group.command('list')
-@cli_context
+@click.option('--group-ids', default=None,
+              help='filter by (comma separated) group-ids')
+@click.option('--group-names', default=None,
+              help='filter by (comma separated) group-names')
 @click.option('--long', is_flag=True,
               help='print out rule info as well')
-def list_security_groups(session, long):
+@cli_context
+def list_security_groups(session, long, group_ids, group_names):
     """List All Security groups."""
     ok, err = EC2SecurityGroupManager(session.get_ec2_session()).\
-        list_security_groups(long)
+        list_security_groups(group_ids, group_names, long)
 
     if not ok:
         print(err)
+
+
+@ec2_security_group.command('create')
+@click.argument('group-name')
+@click.argument('vpc-id')
+@click.option('--description', default=None,
+              help='short description of the security group')
+@cli_context
+def create_security_groups(session, group_name, vpc_id, description):
+    """Create Security group."""
+    ok, err = EC2SecurityGroupManager(session.get_ec2_session()).\
+        create_security_group(group_name, vpc_id, description)
+
+    if not ok:
+        print(err)
+
+
+@ec2_security_group.command('delete')
+@click.argument('group-id-or-name')
+@click.option('--group-name-type', is_flag=True,
+              help='indicates that (comma separated) arguments ' +
+              'passed in are group names')
+@cli_context
+def delete_security_groups(session, group_id_or_name, group_name_type):
+    """Delete Security group."""
+    group_names = group_id_or_name if group_name_type else None
+    group_ids = group_id_or_name if not group_name_type else None
+
+    ok, status = EC2SecurityGroupManager(session.get_ec2_session()).\
+        delete_security_groups(group_ids, group_names)
+
+    print(status)
 
 
 if __name__ == '__main__':
--- awsbot-01/awsbot/ec2_security_group.py
+++ awsbot-01/awsbot/ec2_security_group.py
@@ -5,6 +5,11 @@
 
 from botocore.exceptions import ClientError
 
+try:
+    from awsbot import util
+except ImportError:
+    import util
+
 
 class EC2SecurityGroupManager():
     """EC2 Security Group Manager Class."""
@@ -13,12 +18,12 @@ class EC2SecurityGroupManager():
         """Initialize the ec2 security group manager class."""
         self.ec2_session = ec2_session
 
-    def list_security_groups(self, long=False, pfunc=None):
+    def list_security_groups(self, group_ids=None, group_names=None,
+                             long=False, pfunc=None):
         """List All Security Groups In This region."""
         index = 1
 
         def print_rule(rule_index, rule, spaces=4):
-            """Pretty Print rule associated with security group."""
             port = 'Any-Port'
             from_port = rule.get('FromPort', -1)
             if from_port != -1:
@@ -73,11 +78,75 @@ class EC2SecurityGroupManager():
         if not pfunc:
             pfunc = default_print
 
+        group_id_list = None
+        group_name_list = None
+
+        if group_ids:
+            group_id_list = util.str_to_list(group_ids)
+
+        if group_names:
+            group_name_list = util.str_to_list(group_names)
+
         try:
             for security_group in \
-                    self.ec2_session.get_security_groups():
+                    self.ec2_session.get_security_groups(group_id_list,
+                                                         group_name_list):
                 pfunc(security_group, long)
             return True, None
+        except ClientError as client_err:
+            return False, client_err
+
+    def create_security_group(self, group_name,
+                              vpc_id, description=None):
+        """Create a Security Group."""
+        try:
+            if not description:
+                description = self.ec2_session.\
+                    get_default_security_group_description(group_name)
+            self.ec2_session.\
+                get_ec2_resource().\
+                create_security_group(Description=description,
+                                      GroupName=group_name,
+                                      VpcId=vpc_id)
+            return True, None
+        except ClientError as client_err:
+            return False, client_err
+
+    def delete_security_groups(self, group_ids, group_names, sfunc=None):
+        """Delete Security Groups."""
+        if not group_ids and not group_names:
+            return False, 'Require group_names or group_ids'
+
+        def default_status(status_str):
+            print(status_str)
+
+        if not sfunc:
+            sfunc = default_status
+
+        group_id_list = None
+        group_name_list = None
+
+        if group_ids:
+            group_id_list = util.str_to_list(group_ids)
+
+        if group_names:
+            group_name_list = util.str_to_list(group_names)
+
+        success_count = 0
+        failure_count = 0
+        try:
+            for security_group in self.ec2_session.\
+                    get_security_groups(group_id_list,
+                                        group_name_list):
+                ok, err = self.ec2_session.\
+                        delete_security_group(security_group['GroupId'],
+                                              None, sfunc)
+                if not ok:
+                    sfunc(err)
+                    failure_count += 1
+                else:
+                    success_count += 1
+            return self.ec2_session.get_status(success_count, failure_count)
         except ClientError as client_err:
             return False, client_err
 

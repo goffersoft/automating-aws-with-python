6dd0bb0f7f769bf134f6c5bb5363339e938e146b
goffer123 <goffersoft@gmail.com>
2020-06-24 01:49:41 -0400

    fix word press init script

--- awsbot-01/awsbot/templates/script/wp_script.sh
+++ awsbot-01/awsbot/templates/script/wp_script.sh
@@ -12,6 +12,7 @@ rm -rf latest.tar.gz
 chmod -R 755 wp-content  
 chown -R apache:apache wp-content  
 wget https://s3.amazonaws.com/bucketforwordpresslab-donotdelete/htaccess.txt  
-mv htaccess.txt .htaccess  
+mv htaccess.txt .htaccess
+amazon-linux-extras install php7.2 -y
 service httpd start  
 chkconfig httpd on

691f880e1214f6cc1f1d23028c09bbb7cc46301f
goffer123 <goffersoft@gmail.com>
2020-07-06 20:59:43 -0400

    post message to slack when lambda function is triggered

--- notbot-01/notbot/notifier/handler.py
+++ notbot-01/notbot/notifier/handler.py
@@ -1,7 +1,7 @@
-import json
+import os
+import requests
 
-
-def hello(event, context):
+def post_to_slack(event, context):
     """
     body = {
         "message": "Go Serverless v1.0! Your function executed successfully!",
@@ -16,11 +16,16 @@ def hello(event, context):
     return response
     """
 
+    slack_webhook_url = os.environ['SLACK_WEBHOOK_URL']
+
+    slack_message = 'From {source} at ' + \
+                    '{detail[StartTime]}: {detail[Description]}'
+    slack_message = slack_message.format(**event)
+
+    data = { "text": slack_message }
+    requests.post(slack_webhook_url, json=data)
+
+    print(slack_webhook_url)
     print(event)
 
-    # Use this code if you don't use the http event with the LAMBDA-PROXY
-    # integration
-    return {
-        "message": "Go Serverless v1.0! Your function executed successfully!",
-        "event": event
-    }
+    return

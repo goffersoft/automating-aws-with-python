b6127ca427165e5adec74d0c74efa1ee80f8fcbd
goffer123 <goffersoft@gmail.com>
2020-07-08 13:44:48 -0400

    add pipeline stage to get notification when the video analysis to detect labels is completed; print event for not

--- videobot-01/videolyzer/handler.py
+++ videobot-01/videolyzer/handler.py
@@ -1,5 +1,6 @@
 """Code to do label detection on an image uploaded to S3."""
 
+import os
 import urllib
 import boto3
 
@@ -11,9 +12,16 @@ def start_label_detection(bucket_name, object_key_name):
 
     client = boto3.client('rekognition')
     response = client.start_label_detection(
-        Video={'S3Object': {
-                  'Bucket': bucket_name,
-                  'Name': object_key_name}})
+        Video={
+            'S3Object': {
+                'Bucket': bucket_name,
+                'Name': object_key_name
+            }
+        },
+        NotificationChannel={
+            'SNSTopicArn': os.environ['REKOGNITION_SNS_TOPIC_ARN'],
+            'RoleArn': os.environ['REKOGNITION_SNS_ROLE_ARN']
+        })
 
     print(response)
 
@@ -23,7 +31,7 @@ def start_label_detection(bucket_name, object_key_name):
 def start_processing_video(event, context):
     """Start the video processing pipeline.
 
-    Main Entry point for the AWS LAmbda Function
+    Main Entry point for the AWS Lambda Function
     """
     if not event.get('Records'):
         print(f'No Records to process : {event}')
@@ -33,5 +41,15 @@ def start_processing_video(event, context):
         start_label_detection(
             record['s3']['bucket']['name'],
             urllib.parse.unquote_plus(record['s3']['object']['key']))
+
+    return
+
+
+def handle_label_detection(event, context):
+    """Get and process results from the processing stage.
+
+    Main Entry point for the AWS Lambda Function
+    """
+    print(event)
 
     return
